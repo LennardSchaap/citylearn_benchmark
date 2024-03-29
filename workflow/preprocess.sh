267a870b765f63c9bfdaf37bba2b94611266f0b6
s1914839 <l.l.j.schaap@umail.leidenuniv.nl>
2024-03-29 16:35:24 +0100

    asdf

--- workflow/preprocess.sh
+++ workflow/preprocess.sh
@@ -2,9 +2,9 @@
 
 # size equipment
 
-python src/preprocess.py tx_travis_county_neighborhood summer size_equipment || exit 1
+python src/preprocess.py tx_travis_county_neighborhood_10 summer size_equipment || exit 1
 
 # set schema and work order
 
-python src/preprocess.py tx_travis_county_neighborhood summer set_sb3_work_order travis || exit 1
+python src/preprocess.py tx_travis_county_neighborhood_10 summer set_sb3_work_order travis || exit 1
 
